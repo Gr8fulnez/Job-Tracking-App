1c76e9954c8aebe608daa20f90fca4d538ef7250
New User <laptop@News-MacBook-Pro.local>
2022-10-21 03:36:17 +0100

    feat: Error page and router implement

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { Dashboard, Register, Landing, Error } from "./pages";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Dashboard, Register, Landing, Error} from "./pages";
 
 
 
