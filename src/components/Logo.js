f454d406635dfd135e31210ef5325917e16e8254
New User <laptop@News-MacBook-Pro.local>
2022-10-21 03:17:20 +0100

    feat: A new feature - React router for Dashboard, Landing, Register, and Dashboard

---
+++ src/components/Logo.js
@@ -0,0 +1,7 @@
+import logo from "../assets/images/logo.svg";
+
+const Logo = () => { 
+    <img src={logo} alt="jobify" className="logo" />;
+}
+
+export default Logo;
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,32 +1,33 @@
-import logo from '../assets/images/logo.svg';
-import main from '../assets/images/main.svg';
-import Wrapper from '../assets/css/wrappers/LandingPage';
-
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/css/wrappers/Testing";
+import {Logo}  from "../components";
+import { Link } from "react-router-dom";
 
 const Landing = () => {
   return (
     <Wrapper>
-      <nav>
-        <img src={logo} alt="jobify" className="logo" />
-      </nav>
-
-          <div className="container page">
-              
+          <nav>
+              <Logo />
+          </nav>
+      <div className="container page">
+        {/* info */}
         <div className="info">
           <h1>
-            Job <span>Tracking</span> App
+            job <span>tracking</span> app
           </h1>
-                  <p>
-                      Jobify is a job tracking app that helps you keep track of your job applications.
-                      The app allows you to add, edit, and delete jobs. You can also mark jobs as applied, interviewing, or offer.  
-                  </p>   
-                  <button className='btn btn-hero'>Login/Register</button>  
-              </div>
-              <img src={main} alt="majob junt" id='img main-img' />
+          <p>
+            Jobify is a job tracking app that helps you keep track of your job
+            applications. The app allows you to add, edit, and delete jobs. You
+            can also mark jobs as applied, interviewing, or offer.
+          </p>
+          <Link to="/register" className="btn btn-hero">
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
       </div>
     </Wrapper>
   );
-}
-
+};
 
 export default Landing;
