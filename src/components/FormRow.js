0b2d0e569a52656aea72abaa9d1c256bcdf506bf
New User <laptop@News-MacBook-Pro.local>
2022-10-21 05:03:05 +0100

    feat: Register page form implement

---
+++ src/components/FormRow.js
@@ -0,0 +1,19 @@
+const FormRow = ({ type, handleChange, value, name, labelText }) =>
+{
+  return (
+    <div className="form-row">
+      <label htmlFor={name} className="form-label">
+        {labelText || name}
+      </label> 
+      <input
+        type={type}
+        value={value}
+        name={name}
+        onChange={handleChange}
+        className="form-input"
+      />
+    </div>
+  );
+}
+
+export default FormRow;
