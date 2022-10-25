05754d2130c387ee55ace88c7fcf30e1f8ad735b
New User <laptop@News-MacBook-Pro.local>
2022-10-25 19:42:44 +0100

    feat: Implement email validation

--- server/models/User.js
+++ server/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import validator from 'validator'
 
 const UserSchema = new mongooseSchema({
   name: {
@@ -11,6 +12,10 @@ const UserSchema = new mongooseSchema({
   email: {
     type: String,
     required: [true, "Please provide email"],
+    validate: {
+      validator: validator.isEmail,
+      message: 'Please provide a valid email'
+    },
     unique: true,
   },
   password: {
