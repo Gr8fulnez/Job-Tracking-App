28d658d2fe6423bd2dc30f16d918e5a1aec19b57
New User <laptop@News-MacBook-Pro.local>
2022-10-27 12:32:50 +0100

    feat: Implement error handling

--- server/middleware/error-handler.js
+++ server/middleware/error-handler.js
@@ -1,6 +1,21 @@
+import {StatusCodes} from 'http-status-codes';
+
 const errorHandlerMiddleware = (err, req, res, next) => {
     console.log(err);
-    res.status(500).json({msg: err})
+    const defaultError = {
+        StatusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+        msg: 'Something went wrong, try again later'
+    }
+
+    if (err.name === 'ValidationError') {
+        defaultError.StatusCode = StatusCodes.BAD_REQUEST
+        // defaultError.msg = err.message
+        defaultError.msg = Object.values(err.errors)
+            .map((item) => item.message)
+            .join(', ')
+    }
+    res.status(defaultError.StatusCode).json({msg: defaultError.msg})
+    // res.status(defaultError.StatusCode).json({msg: err})
 }
 
 export default errorHandlerMiddleware
