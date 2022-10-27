8c160241dd5b5690c9ec2455513e4b5d17908783
New User <laptop@News-MacBook-Pro.local>
2022-10-27 11:16:08 +0100

    update: updte register try/catch error using npm express-async-errors

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,12 +1,9 @@
 import User from  "../models/User.js";
 
 const register = async (req, res, next) => {
-  try {
+ 
     const user = await User.create(req.body)
     res.status(201).json({ user });
-  } catch (error) {
-    next(error)
-  }
 }
 
 
