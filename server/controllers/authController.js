b609671ceb3b2074bb09b064b32e9d8a84022431
New User <laptop@News-MacBook-Pro.local>
2022-10-27 10:43:37 +0100

    feat: implement error handler

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,17 +1,14 @@
 import User from  "../models/User.js";
 
-const register = async (req, res) => {
+const register = async (req, res, next) => {
   try {
     const user = await User.create(req.body)
     res.status(201).json({ user });
   } catch (error) {
-    res.status(500).json({ msg: "there was an error" });
+    next(error)
   }
 }
 
-// const register = async (req, res) => {
-//   res.send("register user");
-// };
 
 const login = async (req, res) => {
   res.send("login user");
