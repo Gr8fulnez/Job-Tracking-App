b897457a3d1b68aa0ae2315955d6ed862a986173
New User <laptop@News-MacBook-Pro.local>
2022-10-29 18:21:16 +0100

    feat: Implement mogan

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const app = express();
 import dotenv from "dotenv";
 dotenv.config();
 import 'express-async-errors'
+import morgan from "morgan";
 
 //db and authenticateUser
 import connectDB from "./db/connect.js";
@@ -16,6 +17,9 @@ import jobsRouter from "./routes/jobsRoutes.js";
 import notFoundMiddleware from "./middleware/not-found.js";
 import errorHandlerMiddleware from "./middleware/error-handler.js";
 
+if (process.env.NODE_ENV !== 'production') {
+  app.use(morgan('dev'))
+}
 app.use(cors())
 app.use(express.json())
 
@@ -24,6 +28,10 @@ app.use(express.json())
 app.get("/", (req, res) => {
   res.json({ msg: "Welcome!" });
 });
+app.get("/api/v1", (req, res) => {
+  res.json({ msg: "API!" }); 
+});
+
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", jobsRouter);
