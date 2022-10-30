f982ab234de279620530140d99784a6bea6da752
New User <laptop@News-MacBook-Pro.local>
2022-10-30 02:45:26 +0100

    feat: Implement login connection on the client side

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -9,6 +9,9 @@ import {
   REGISTER_USER_BEGIN,
   REGISTER_USER_SUCCESS,
   REGISTER_USER_ERROR,
+  LOGIN_USER_BEGIN,
+  LOGIN_USER_SUCCESS,
+  LOGIN_USER_ERROR,
 } from "./action";
 
 const token = localStorage.getItem('token')
@@ -79,9 +82,34 @@ const AppProvider = ({ children }) => {
         clearAlert()
     }
 
+    const loginUser = async (currentUser) => {
+        // console.log(currentUser);
+        dispatch({ type: LOGIN_USER_BEGIN });
+        try {
+            const {data} = await axios.post('/api/v1/auth/login', currentUser)
+            const { user, token, location } = data 
+            dispatch({
+                type: LOGIN_USER_SUCCESS,
+                payload: {
+                    user,
+                    token,
+                    location
+                }
+            })
+            addUserToLocalStorage({ user, token, location })
+        } catch (error) {
+            dispatch({
+                type: LOGIN_USER_ERROR,
+                payload: {msg: error.response.data.msg}
+            })
+        }
+        clearAlert()
+    }
+
+   
 
     return (
-      <AppContext.Provider value={{ ...state, displayAlert, registerUser }}>
+      <AppContext.Provider value={{ ...state, displayAlert, registerUser, loginUser }}>
         {children}
       </AppContext.Provider>
     );
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -15,7 +15,7 @@ const initialState = {
 const Register = () => {
     const navigate = useNavigate()
     const [values, setValues] = useState(initialState);
-    const {user, isLoading, showAlert, displayAlert, registerUser} = useAppContext()
+    const {user, isLoading, showAlert, displayAlert, registerUser, loginUser} = useAppContext()
 
 
     const toggleMember = () => {
@@ -36,7 +36,7 @@ const Register = () => {
     
     const currentUser = { name, email, password }
     if (isMember) {
-      console.log('already a member')
+      loginUser(currentUser)
     }
     else{
       registerUser(currentUser)
