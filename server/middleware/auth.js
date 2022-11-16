a71d2aab99514c3976ae246c7381f99a1c496084
New User <laptop@News-MacBook-Pro.local>
2022-11-16 08:37:24 +0100

    feat: authenticatation setup complete

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,6 +1,24 @@
+import jwt from 'jsonwebtoken'
+import { UnauthenticatedError } from "../error/index.js";
+
+UnauthenticatedError;
 const auth = async (req, res, next) => {
-    console.log('authenticate user')
-    next()
+const authHeader = req.headers.authorization;
+if (!authHeader || !authHeader.startsWith("Bearer")) {
+  throw new UnauthenticatedError("Authentication invalid");
+}
+const token = authHeader.split(" ")[1];
+
+try {
+  const payload = jwt.verify(token, process.env.JWT_SECRET);
+//   console.log(payload)
+  // attach the user request object
+//   req.user = payload
+  req.user = { userId: payload.id };
+  next();
+} catch (error) {
+  throw new UnauthenticatedError("Authentication invalid");
+}
 }
 
 export default auth 
